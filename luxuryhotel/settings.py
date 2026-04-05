ce562c022b50229ac89433f2195fa86332d0421e
Godson Oyibayado <godsonoyibayado@gmail.com>
2026-04-05 11:57:00 +0100

    fix csrf trusted origins default

--- luxuryhotel/settings.py
+++ luxuryhotel/settings.py
@@ -30,7 +30,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
 
-CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='').split(',')
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='https://t-luxury-haven-hotel-production.up.railway.app').split(',')
 
 # Application definition
 
