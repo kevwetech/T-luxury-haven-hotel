bfcdf2a452387d5c2ea79a39793a0adb8b301ce1
Godson Oyibayado <godsonoyibayado@gmail.com>
2026-04-06 07:50:10 +0100

    fix static root path

--- luxuryhotel/settings.py
+++ luxuryhotel/settings.py
@@ -137,7 +137,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/app/staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
