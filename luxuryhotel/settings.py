f735db8de621e716e61c8f0e7dcca7bfd44b3578
Godson Oyibayado <godsonoyibayado@gmail.com>
2026-04-07 18:24:24 +0100

    fix cloudinary installed apps

--- luxuryhotel/settings.py
+++ luxuryhotel/settings.py
@@ -40,12 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.humanize',
-    'cloudinary_storage',
+    'cloudinary_storage',        # ← before staticfiles
     'django.contrib.staticfiles',
     'cloudinary',
+    'django.contrib.humanize',
 
+    
     'rooms',
     'accounts',
 ]
